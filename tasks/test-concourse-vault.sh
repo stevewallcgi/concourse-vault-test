e7be6a9c9f4882defd8fb257e559bf6996ce3bf2
Steve Wall <swall@ecsteam.com>
2017-09-11 11:47:20 -0600

    wip

--- tasks/test-concourse-vault.sh
+++ tasks/test-concourse-vault.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if [ $VAULT_RETRIEVED == "admin " ]; then
+set -x
+
+echo "started $VAULT_RETRIEVED"
+
+if [ "$VAULT_RETRIEVED" == "admin " ]; then
   # Successfully retrieved value from vault
+  echo "Success"
   exit 0
 fi
 
 # Failed to retrieve expected value from vault.
+echo "Failed"
 exit 1
